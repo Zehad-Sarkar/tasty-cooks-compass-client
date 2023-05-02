349f3c2905b8796025816c52a51a26680f7b4893
Zehad-Sarkar <zehadsarkar22@gmail.com>
2023-05-02 10:37:50 +0600

    remove firebase secret key by git push

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,23 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-console.log(import.meta.env.VITE_APIKEY)
+console.log(import.meta.env.VITE_APIKEY);
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_APIKEY,
-  authDomain:import.meta.env.VITE_AUTHDOMAIN,
-  projectId:import.meta.env.VITE_PROJECTID,
-  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
-  appId:import.meta.env.VITE_APPID,
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAa6QHXJRcb95rJBnXkdGfmo1CCFQbJR4k",
-//   authDomain: "tasty-cookes-compass.firebaseapp.com",
-//   projectId: "tasty-cookes-compass",
-//   storageBucket: "tasty-cookes-compass.appspot.com",
-//   messagingSenderId: "731418475509",
-//   appId: "1:731418475509:web:f57b7997e0b0b825a27ebc",
-// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
